082be04768c90666b85fc186d44b0bde75da73e8
eligigante <elijahg975@gmail.com>
2024-07-05 20:26:17 +0800

    add new smthing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+from textblob import TextBlob
 
 column1, column2 = st.columns([1, 9])\
 
@@ -16,7 +17,22 @@ text = st.text_area("Article: ", placeholder="Input text here")
 st.markdown('<p style="font-style: italic; color: gray;">Note: Please note that it may sometimes produce inaccurate results.</p>', unsafe_allow_html=True)
 
 # code for bert or whatever
-# if text:
+if text:
+    blob = TextBlob(text)
+    polarity = round(blob.sentiment.polarity, 2)
+    subjectivity = round(blob.sentiment.subjectivity, 2)
+    st.write('Polarity: ', polarity)
+    st.write('Subjectivity: ', subjectivity)
+
+    if polarity >= 0.5:
+        st.markdown('<div style="text-align:center;"><span style="font-size:100px;">😊</span><p style="font-size:24px;">Happy!</p></div>', unsafe_allow_html=True)
+    elif polarity <= -0.5:
+        st.markdown('<div style="text-align:center;"><span style="font-size:48px;">😞</span><p style="font-size:24px;">Sad!</p></div>', unsafe_allow_html=True)
+    else:
+        st.markdown('<div style="text-align:center;"><span style="font-size:48px;">😐</span><p style="font-size:24px;">Neutral!</p></div>', unsafe_allow_html=True)
+
+
+
 
 
 with st.expander("About the creators", icon='ℹ'):
